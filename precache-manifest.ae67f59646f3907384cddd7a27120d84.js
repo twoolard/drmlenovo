84b27fd6bd2d894450ae5759e6f0f650402c792f
Wes Hatley <weshatley@gmail.com>
2019-06-14 10:59:23 -0400

    Updates

---
+++ precache-manifest.ae67f59646f3907384cddd7a27120d84.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "028b0ab877e41ede43ec",
+    "url": "/drmlenovo/static/css/main.a6c709b1.chunk.css"
+  },
+  {
+    "revision": "028b0ab877e41ede43ec",
+    "url": "/drmlenovo/static/js/main.028b0ab8.chunk.js"
+  },
+  {
+    "revision": "332306f01e4d680f4e42",
+    "url": "/drmlenovo/static/css/1.62b48be7.chunk.css"
+  },
+  {
+    "revision": "332306f01e4d680f4e42",
+    "url": "/drmlenovo/static/js/1.332306f0.chunk.js"
+  },
+  {
+    "revision": "233c7dc6adb4b61239b1",
+    "url": "/drmlenovo/static/js/runtime~main.233c7dc6.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/drmlenovo/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "eee899f3585ee4f8a5d7db928aceb555",
+    "url": "/drmlenovo/static/media/anime6.eee899f3.png"
+  },
+  {
+    "revision": "4ece6b021623b8a0c192f0b31b2bdebe",
+    "url": "/drmlenovo/static/media/Headshot.4ece6b02.jpg"
+  },
+  {
+    "revision": "9b1b235a3ac5f1e20e521e0e552a2050",
+    "url": "/drmlenovo/static/media/805197.9b1b235a.jpg"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/drmlenovo/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/drmlenovo/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/drmlenovo/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/drmlenovo/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/drmlenovo/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "e399c7f9e144ba323c404f01dbef84c7",
+    "url": "/drmlenovo/index.html"
+  }
+];
