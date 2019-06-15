20ed97b9858ff40b0864bb60c2408db9a8b58b6f
Wes Hatley <weshatley@gmail.com>
2019-06-14 20:32:34 -0400

    Updates

---
+++ precache-manifest.911c3485eb4d491ef6de128a1097647c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2cdd61353400cd95290396ff147294",
+    "url": "/drmlenovo/index.html"
+  },
+  {
+    "revision": "a3669b97aeb2cc8459ef",
+    "url": "/drmlenovo/static/css/main.9b678abe.chunk.css"
+  },
+  {
+    "revision": "863dc6f19a5c478e7119",
+    "url": "/drmlenovo/static/js/2.e354aa8d.chunk.js"
+  },
+  {
+    "revision": "a3669b97aeb2cc8459ef",
+    "url": "/drmlenovo/static/js/main.9a1ce1e5.chunk.js"
+  },
+  {
+    "revision": "eea8b37b22a7be5cccbe",
+    "url": "/drmlenovo/static/js/runtime~main.db1a3394.js"
+  },
+  {
+    "revision": "9b1b235a3ac5f1e20e521e0e552a2050",
+    "url": "/drmlenovo/static/media/Background.9b1b235a.jpg"
+  },
+  {
+    "revision": "4ece6b021623b8a0c192f0b31b2bdebe",
+    "url": "/drmlenovo/static/media/Headshot.4ece6b02.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/drmlenovo/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/drmlenovo/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/drmlenovo/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/drmlenovo/static/media/nucleo.f0b489a5.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drmlenovo/precache-manifest.4cc005e3fb387a4bbb8e50ab20c96ee3.js"
+  "/drmlenovo/precache-manifest.911c3485eb4d491ef6de128a1097647c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/drmlenovo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drmlenovo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
